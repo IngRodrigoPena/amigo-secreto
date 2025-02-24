1233df625e5e0bd474f41de1536e214e08cdfe4f
IngRodrigoPena <ingrodrigopena@gmail.com>
2025-02-24 13:43:43 -0600

    se agrego funcion que actualiza la lista de amigos

--- app01.js
+++ app01.js
@@ -9,7 +9,7 @@ function agregarAmigo(){
     // verifica que la variable nombre contenga una cadena vacia
     //si es una cadena vacia mostar un mensaje 
     //trim quita posibles espacios en blanco
-    if(nombre.trim === ""){
+    if(nombre.trim() === ""){
         alert("Por favor insete un nombre");
     }else{
         //agrega el nombre a la lista
@@ -18,5 +18,19 @@ function agregarAmigo(){
        document.getElementById('amigo').value = "";     
     }
     //console.log(nombre);agregar
-    console.log(`lista de amigos:o :${lista_amigos.toString()}`);
+    //console.log(`lista de amigos:o :${lista_amigos.toString()}`);
+    mostrarLista(lista_amigos);
+}
+
+function mostrarLista(lista){
+    let listaElement = document.getElementById("listaAmigos");
+    //limpia la lista antes de actualizar
+    listaElement.innerHTML = "";
+    //recorre el arreglo de los nombres de amigos y los muestra en una lista
+    for(let i=0; i<lista.length;i++){
+        let item = document.createElement("li");
+        item.textContent = `${i+1}. ${lista[i]}`
+        listaElement.appendChild(item);
+        //console.log(`${i}  ${lista[i]}`);
+    }
 }
